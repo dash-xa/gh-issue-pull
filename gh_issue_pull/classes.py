c1c112e1d1fbbf9e5f1eb0824a7222e5fc1bf503
Alex Shevchuk <shevrlex@gmail.com>
2024-10-21 12:19:39 -0700

    Source issues from random GitHub repos (#4)
    * Add garbage code to sample issues from random python repos
    
    * Add vercel.json

--- gh_issue_pull/classes.py
+++ gh_issue_pull/classes.py
@@ -14,3 +14,19 @@ class SWEBenchEntry(BaseModel):
     test_patch: str
     FAIL_TO_PASS: List[str]
     PASS_TO_PASS: List[str]
+
+
+class SWEBenchEntryWithoutTests(BaseModel):
+    repo_name: str
+    problem_statement: str
+    patch: str
+    base_commit: str
+
+
+def convert_swedataset_item_to_entry(issue: dict) -> SWEBenchEntryWithoutTests:
+    return SWEBenchEntryWithoutTests(
+        repo_name=issue["title"],
+        problem_statement=issue["topic"],
+        patch=issue["content"],
+        base_commit=issue["extras"]["base_commit"],
+    )
--- gh_issue_pull/main.py
+++ gh_issue_pull/main.py
@@ -1,16 +1,20 @@
 from contextlib import asynccontextmanager
+from typing import Optional
 
 from fastapi import FastAPI, HTTPException
 
+from gh_issue_pull.classes import convert_swedataset_item_to_entry
 from gh_issue_pull.helpers import fetch_unused_issue, pull_all_swebench_entries, SWEBENCH_DB_PATH, obfuscate
+from gh_issue_pull.swebench_dataset import SWEDataset
 
+
+dataset: Optional[SWEDataset] = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("App is starting up")
-    if not SWEBENCH_DB_PATH.exists():
-        pull_all_swebench_entries()
-
+    global dataset
+    dataset = SWEDataset()
     yield  # The app runs between startup and shutdown here
 
     print("App is shutting down")
@@ -25,9 +29,8 @@ async def get_task():
     Gets the current problem to be solved by validators, as a ProblemStatement object
     """
     try:
-        unused_issue = fetch_unused_issue()
-        unused_issue.problem_statement = obfuscate(unused_issue)
-        return unused_issue.model_dump()
+        item = dataset.get(1)
+        return convert_swedataset_item_to_entry(item)
     except Exception as e:
         print("Internal server error:", e)
         raise HTTPException(status_code=500)
@@ -37,5 +40,3 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-# Pull codebases to provide oracle files
